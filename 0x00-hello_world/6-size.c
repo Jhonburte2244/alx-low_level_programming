126844270abdae1c21f374e51146798e82a9f291
Jhonburte2244 <jhonburte2244@gmail.com>
2023-03-21 00:57:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+ 
 /**
- * main -Entry point
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
  *
- * Return: Always 0(succeess)
+ * Return: Always 0.
  */
+
 int main(void)
 {       
-        printf("size of a char %d byte(s):\n " sizeof(char));
+        printf("Size of a char: %zu byte(s)\n", sizeof(char));
 
-	printf("size of an int %d byte(s):\n " sizeof(int));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 
-	printf("size of long int  %d byte(s):\n " sizeof(long int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 
-	printf("size of long long int %d byte(s):\n " sizeof(long long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 
-	printf("size of float %d byte(s):\n " sizeof(float));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-        return (0); 
+        return (0);
+
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,2 +1,3 @@
-[master 95a64e5] 5-printf.c
- 2 files changed, 4 insertions(+), 3 deletions(-)
+[master 6516a19] 6-size.c
+ 2 files changed, 22 insertions(+), 3 deletions(-)
+ create mode 100755 0x00-hello_world/6-size.c
