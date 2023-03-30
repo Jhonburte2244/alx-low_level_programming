d41a065eb25d3a14c990a667297c556351f8ee08
Jhonburte2244 <jhonburte2244@gmail.com>
2023-03-30 03:56:06 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,28 @@
+#ifndef main_h
+#define main_h
+
+/* This file constains all the prototypes of the projects */
+/* t0. reset_to_98 - this function reset all to 98 */
+void reset_to_98(int *n);
+/* t1. swap_int - this function change the numbers */
+void swap_int(int *a, int *b);
+/* t2. _strlen - this the function count the nombers of characters */
+int _strlen(char *s);
+/* t3. _puts - this the function print a array of string */
+void _puts(char *str);
+/* t4. print_rev - this the function print a array of string in reverse. */
+void print_rev(char *s);
+/* t5. rev_string - print a reverse strings. */
+void rev_string(char *s);
+/* t6. puts2 - this function print only the even numbers.*/
+void puts2(char *str);
+/* t7. puts_half - this function print only the even numbers. */
+void puts_half(char *str);
+/* t8. print_array - this fuction print an array */
+void print_array(int *a, int n);
+/* t9. _strcpy - this fuction cope the pointer and array */
+char *_strcpy(char *dest, char *src);
+/* _purchar - puthcar fuction */
+int _putchar(char c);
+#endif
+
