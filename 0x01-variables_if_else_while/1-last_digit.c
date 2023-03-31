e27bb2203266e210012b10bb4be96c1a4fa2c840
Jhonburte2244 <jhonburte2244@gmail.com>
2023-03-31 00:04:51 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,26 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
-	int m; 
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10; 
+	last = n % 10;
 
-	if (m  > 5)
-	
-		printf("Last digit of %d is %d and is greater than 5 \n", n, m);
-	
-	else if (m == 0)
-	
-		printf("Last digit of %d is %d and is  0  \n" , n, m);
-	
-	else if ( m < 6 &&m != 0)
-	
-		printf("Last digit of %d is %d and is less than 6 and not  0\n", n, m);
-
-	printf("\n");
+	if (last > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,2 @@
-[master ec39f63] 0-positive_or_negative.c
- 3 files changed, 26 insertions(+), 29 deletions(-)
- rewrite 0x01-variables_if_else_while/0-positive_or_negative.c (81%)
+[master b782a6d] 0-positive_or_negative.c
+ 1 file changed, 3 insertions(+), 1 deletion(-)
