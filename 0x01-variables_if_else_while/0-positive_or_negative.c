ec39f6359c87d6e2dc0600d750c27fd2b21ac21e
Jhonburte2244 <jhonburte2244@gmail.com>
2023-03-31 00:00:12 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if(n>0)
-        {
-                printf("%d is posative \n", n);
-        }
-        else if(n==0)
-        {
-                printf("%d is Zero \n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
 
-        }
-        else
-        {
-                printf("%d is negative \n", n);
-        }
-        return (0);
+	return (0);
 }
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,4 +1,1 @@
-[master 2a0b7e2] 3-print_alphabets
- 3 files changed, 20 insertions(+), 5 deletions(-)
- create mode 100755 0x01-variables_if_else_while/3-print_alphabets
- create mode 100755 0x01-variables_if_else_while/3-print_alphabets.c
+0x01. C - Variables, if, else, while
